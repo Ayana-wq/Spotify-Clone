3df2f0d8c594236edc96a004b0799f1ddfcd3882
Ayana-wq <60111729+Ayana-wq@users.noreply.github.com>
2024-11-06 07:56:39 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar({ navBackground }) {
     <Container navBackground={navBackground}>
       <div className="search__bar">
         <FaSearch />
-        <input type="text" placeholder="Artists, songs, or podcasts" />
+        <input type="text" placeholder=" Artists, songs, or podcasts" />
       </div>
       <div className="avatar">
         <a href={userInfo?.userUrl}>
